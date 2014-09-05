6a5d1d60cef9f899b8444d3d825db951e5e1edaa
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2014-09-05 14:28:17 +0900

    roomScene用にコードを変更

--- preCyberIntern/roomScene.swift
+++ preCyberIntern/roomScene.swift
@@ -1,9 +1,22 @@
 //
-//  roomScene.swift
+//  GameScene.swift
 //  preCyberIntern
 //
-//  Created by ucuc on 9/5/14.
+//  Created by ucuc on 9/4/14.
 //  Copyright (c) 2014 ucuc. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+    }
+}
